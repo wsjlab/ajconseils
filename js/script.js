a652e170fc96c33109112beb8421d1482c9fe565
wsjlab <f.mezzane@gmail.com>
2024-12-27 17:59:36 +0100

    az

--- js/script.js
+++ js/script.js
@@ -144,3 +144,12 @@ mybutton.addEventListener("click",function(){
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  var menuItems = document.querySelectorAll('.nav-item');
+  menuItems.forEach(function(item) {
+      item.addEventListener('click', function() {
+          var menu = document.querySelector('.navbar-nav');
+          menu.style.display = 'none';
+      });
+  });
+});
